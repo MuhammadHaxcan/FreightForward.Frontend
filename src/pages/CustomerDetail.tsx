dad1a9224aebd8dfdf4d2f549df514f4fac2531d
Muhammad Hassan <muhammadhaxcan@gmail.com>
2026-01-15 22:25:09 +0500

    FIXED

--- src/pages/CustomerDetail.tsx
+++ src/pages/CustomerDetail.tsx
@@ -14,6 +14,7 @@ import { useParams, useNavigate, useSearchParams } from "react-router-dom";
 import { cn, formatDate } from "@/lib/utils";
 import { customerApi, settingsApi, CustomerCategoryType, Currency, Invoice as ApiInvoice, AccountReceivable as ApiAccountReceivable, PaymentStatus } from "@/services/api";
 import { useToast } from "@/hooks/use-toast";
+import { format } from "date-fns";
 
 interface Contact {
   id: number;
